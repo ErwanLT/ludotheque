4fb510d8f36738d545bad8fdaed3363ea5807ab6
Erwan <erwanletutour.elt@gmail.com>
2024-08-23 16:05:43 +0200

    switch css theme

--- src/main/java/fr/eletutour/ludotheque/views/MainLayout.java
+++ src/main/java/fr/eletutour/ludotheque/views/MainLayout.java
@@ -1,15 +1,21 @@
 package fr.eletutour.ludotheque.views;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.Lumo;
+
 
 public class MainLayout extends AppLayout {
+    Button toggleThemeButton;
 
     public MainLayout() {
         createHeader();
@@ -19,17 +25,37 @@ public class MainLayout extends AppLayout {
     private void createHeader() {
         H1 logo = new H1("Ma ludothèque");
 
+        // Spacer pour pousser les composants suivants à droite
+        HorizontalLayout spacer = new HorizontalLayout();
+        spacer.setWidthFull();
+
+        // Bouton pour basculer entre les thèmes
+        toggleThemeButton = new Button("Thème sombre", click -> {
+            ThemeList themeList = UI.getCurrent().getElement().getThemeList();
+
+            if (themeList.contains(Lumo.DARK)) {
+                themeList.remove(Lumo.DARK);
+                toggleThemeButton.setText("Thème sombre");
+            } else {
+                themeList.add(Lumo.DARK);
+                toggleThemeButton.setText("Thème clair");
+            }
+        });
+
+        // Header layout avec logo, spacer et bouton de bascule du thème
         HorizontalLayout header = new HorizontalLayout(
                 new DrawerToggle(),
-                logo
+                logo,
+                spacer,          // Ajouter le spacer ici pour pousser le bouton à droite
+                toggleThemeButton
         );
 
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         header.setWidth("100%");
         header.addClassNames("py-0", "px-m");
 
+        // Ajouter le header à la barre de navigation
         addToNavbar(header);
-
     }
 
     private void createDrawer() {
