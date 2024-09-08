1676d3a1f1c9fab1159bf355cbed3ec8c0dbbd41
Erwan <erwanletutour.elt@gmail.com>
2024-09-08 08:57:05 +0200

    boom baby
    https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWgxOWhnOTBnYTVueHJncDljbWV3cXR5ajdjcGF5MDgwYmJ2aDl1OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ANYoDAeywnsBi/giphy.gif
    
    PWA

--- src/main/java/fr/eletutour/ludotheque/LudothequeApplication.java
+++ src/main/java/fr/eletutour/ludotheque/LudothequeApplication.java
@@ -1,21 +1,27 @@
 package fr.eletutour.ludotheque;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
-import fr.eletutour.ludotheque.dao.repository.JeuSocieteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 
 @SpringBootApplication
 @Theme("my-theme")
+@PWA(
+		name = "Ludotheque",
+		shortName = "LudoApp",
+		iconPath = "images/icon.png",
+		description = "Application de gestion de ludothèque",
+		offlinePath = "offline.html",
+		offlineResources = {"images/icon.png"}
+)
 public class LudothequeApplication implements AppShellConfigurator {
 
-	@Autowired
-	private JeuSocieteRepository jeuSocieteRepository;
-
 	public static void main(String[] args) {
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "production");
 		SpringApplication.run(LudothequeApplication.class, args);
 	}
 
